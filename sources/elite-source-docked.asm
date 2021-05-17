318a656d04e9ff90c65d30e97eab4e5bc3ff0ef4
Mark Moxon <mark@moxon.net>
2021-05-17 17:15:38 +0100

    Fixed incorrect TP variable documentation

--- sources/elite-source-docked.asm
+++ sources/elite-source-docked.asm
@@ -1194,8 +1194,21 @@ ORG &0300
 
 .TP
 
- SKIP 1                 \ The current mission status, which is always 0 for the
-                        \ cassette version of Elite as there are no missions
+ SKIP 1                 \ The current mission status:
+                        \
+                        \   * Bits 0-1 = Mission 1 status
+                        \
+                        \     * %00 = Mission not started
+                        \     * %01 = Mission in progress, hunting for ship
+                        \     * %11 = Constrictor killed, not debriefed yet
+                        \     * %10 = Mission and debrief complete
+                        \
+                        \   * Bits 2-3 = Mission 2 status
+                        \
+                        \     * %00 = Mission not started
+                        \     * %01 = Mission in progress, plans not picked up
+                        \     * %10 = Mission in progress, plans picked up
+                        \     * %11 = Mission complete
 
 .QQ0
 
