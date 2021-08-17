99170025e3ba365a60c85cf4c06d5b9ec5cc679b
Mark Moxon <mark@moxon.net>
2021-08-17 16:37:10 +0100

    Rewording of comments in docked code checksum data block

--- sources/elite-source-docked.asm
+++ sources/elite-source-docked.asm
@@ -31947,10 +31947,10 @@ ENDMACRO
 
 \ ******************************************************************************
 \
-\       Name: Unused block 2
+\       Name: Checksum
 \       Type: Variable
-\   Category: Utility routines
-\    Summary: These bytes appear to be unused
+\   Category: Copy protection
+\    Summary: Contains a checksum at &55FF that is checked by the loader
 \
 \ ******************************************************************************
 
@@ -31968,7 +31968,10 @@ IF _STH_DISC
  EQUB &00, &00
  EQUB &06, &56
  EQUB &52, &49
- EQUB &45, &E6
+ EQUB &45
+
+ EQUB &E6               \ This checksum is at location &55FF, and is checked in
+                        \ the LOAD routine in elite-loader3.asm
 
 ELIF _IB_DISC
 
@@ -31984,7 +31987,10 @@ ELIF _IB_DISC
  EQUB &52, &49
  EQUB &53, &00
  EQUB &8E, &13
- EQUB &34, &B3
+ EQUB &34
+
+ EQUB &B3               \ This checksum is at location &55FF, and is checked in
+                        \ the LOAD routine in elite-loader3.asm
 
 ENDIF
 
