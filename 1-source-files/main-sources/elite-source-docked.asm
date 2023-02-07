1c9359aa7a19d6361391eba0c89cf39e076d8a44
Mark Moxon <mark@moxon.net>
2023-02-07 19:39:52 +0000

    Added Mod: headers to moved code

--- 1-source-files/main-sources/elite-source-docked.asm
+++ 1-source-files/main-sources/elite-source-docked.asm
@@ -26750,13 +26750,17 @@ LOAD_G% = LOAD% + P% - CODE%
                         \ screen, so jump to LL78 so we don't store this line
                         \ in the ship line heap
 
-                        \ --- Mod: Code added for flicker-free Elite: --------->
+                        \ --- Mod: Original Acornsoft code removed: ----------->
+
+\JMP LL80               \ Jump down to part 11 to draw this edge
+
+                        \ --- And replaced by: -------------------------------->
 
  JSR LLX30              \ Draw this edge using smooth animation, by first
                         \ drawing the ship's new line and then erasing the
                         \ corresponding old line from the screen
 
-                        \ --- End of added code ------------------------------->
+                        \ --- End of replacement ------------------------------>
 
 \ ******************************************************************************
 \
@@ -27384,6 +27388,98 @@ LOAD_G% = LOAD% + P% - CODE%
                         \ fit on-screen
 
  RTS                    \ Return from the subroutine
+
+\ ******************************************************************************
+\
+\       Name: LL9 (Part 11 of 12), Moved
+\       Type: Subroutine
+\   Category: Drawing ships
+\    Summary: Draw ship: Add all visible edges to the ship line heap
+\  Deep dive: Drawing ships
+\
+\ ------------------------------------------------------------------------------
+\
+\ This part adds all the visible edges to the ship line heap, so we can draw
+\ them in part 12.
+\
+\ Other entry points:
+\
+\   LL81+2              Draw the contents of the ship line heap, used to draw
+\                       the ship as a dot from SHPPT
+\
+\ ******************************************************************************
+
+                        \ --- Mod: Original Acornsoft code moved elsewhere: --->
+
+\.LL80
+\
+\LDY U                  \ Fetch the ship line heap pointer, which points to the
+\                       \ next free byte on the heap, into Y
+\
+\LDA XX15               \ Add X1 to the end of the heap
+\STA (XX19),Y
+\
+\INY                    \ Increment the heap pointer
+\
+\LDA XX15+1             \ Add Y1 to the end of the heap
+\STA (XX19),Y
+\
+\INY                    \ Increment the heap pointer
+\
+\LDA XX15+2             \ Add X2 to the end of the heap
+\STA (XX19),Y
+\
+\INY                    \ Increment the heap pointer
+\
+\LDA XX15+3             \ Add Y2 to the end of the heap
+\STA (XX19),Y
+\
+\INY                    \ Increment the heap pointer
+\
+\STY U                  \ Store the updated ship line heap pointer in U
+\
+\CPY T1                 \ If Y >= T1 then we have reached the maximum number of
+\BCS LL81               \ edge lines that we can store in the ship line heap, so
+\                       \ skip to LL81 so we don't loop back for the next edge
+\
+\.LL78
+\
+\INC XX17               \ Increment the edge counter to point to the next edge
+\
+\LDY XX17               \ If Y >= XX20, which contains the number of edges in
+\CPY XX20               \ the blueprint, jump to LL81 as we have processed all
+\BCS LL81               \ the edges and don't need to loop back for the next one
+\
+\LDY #0                 \ Set Y to point to byte #0 again, ready for the next
+\                       \ edge
+\
+\LDA V                  \ Increment V by 4 so V(1 0) points to the data for the
+\ADC #4                 \ next edge
+\STA V
+\
+\BCC ll81               \ If the above addition didn't overflow, jump to ll81
+\
+\INC V+1                \ Otherwise increment the high byte of V(1 0), as we
+\                       \ just moved the V(1 0) pointer past a page boundary
+\
+\.ll81
+\
+\JMP LL75               \ Loop back to LL75 to process the next edge
+\
+\.LL81
+\
+\                       \ We have finished adding lines to the ship line heap,
+\                       \ so now we need to set the first byte of the heap to
+\                       \ the number of bytes stored there
+\
+\LDA U                  \ Fetch the ship line heap pointer from U into A, which
+\                       \ points to the end of the heap, and therefore contains
+\                       \ the heap size
+\
+\LDY #0                 \ Store A as the first byte of the ship line heap, so
+\STA (XX19),Y           \ the heap is now correctly set up
+
+                        \ --- End of moved code ------------------------------->
 
 \ ******************************************************************************
 \
