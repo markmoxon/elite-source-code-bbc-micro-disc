d24eedd01b72d6477a38f66351d7aac7e29becab
Mark Moxon <mark@moxon.net>
2023-02-20 18:43:05 +0000

    Convert hard-coded address to label in loaderConvert hard-coded address to label in loader

--- 1-source-files/main-sources/elite-loader3.asm
+++ 1-source-files/main-sources/elite-loader3.asm
@@ -1147,13 +1147,13 @@ ORG CATDcode + P% - CATD
  LDA #&63
  STA ZP+1
 
- LDA #&62               \ Set P(1 0) = &2A62
+ LDA #LO(ELITE)         \ Set P(1 0) = ELITE
  STA P
- LDA #&2A
+ LDA #HI(ELITE)
  STA P+1
 
  LDX #8                 \ Call MVPG with X = 8 to copy 8 pages of memory from
- JSR MVPG               \ the address in P(1 0) to the address in ZP(1 0)
+ JSR MVPG               \ ELITE to &6300
 
 \ ******************************************************************************
 \
