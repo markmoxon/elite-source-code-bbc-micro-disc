8dcc1ce83fc1b252cc17850d368d6df9827d915e
Mark Moxon <mark@moxon.net>
2023-03-14 19:21:14 +0000

    Fix typo in flicker-free planets commentary

--- 1-source-files/main-sources/elite-source-flight.asm
+++ 1-source-files/main-sources/elite-source-flight.asm
@@ -5909,8 +5909,8 @@ NEXT
 \
 \.nlin1
 \
-\                       \ If we get here then the old line is differnt to the new
-\                       \ line, so we draw them both
+\                       \ If we get here then the old line is different to the
+\                       \ new line, so we draw them both
 \
 \JSR LL30               \ Draw the new line from (X1, Y1) to (X2, Y2)
 \
