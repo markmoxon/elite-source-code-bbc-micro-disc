56f4bcc93ae6944a18a294afe6f85268d598b75f
Mark Moxon <mark@moxon.net>
2023-03-21 11:04:35 +0000

    Update Saturn commentary

--- 1-source-files/main-sources/elite-loader3.asm
+++ 1-source-files/main-sources/elite-loader3.asm
@@ -1101,8 +1101,8 @@ ORG CATDcode + P% - CATD
  BCC PLC3
 
  TYA                    \ Set A = Y + T
- ADC T                  \       = r7^2 / 256 + r6^2 / 256
-                        \       = (r6^2 + r7^2) / 256
+ ADC T                  \       = (r6 + r7)^2 / 256 + r6^2 / 256
+                        \       = ((r6 + r7)^2 + r6^2) / 256
 
  CMP #16                \ If A >= 16, skip to PL1 to plot the pixel
  BCS PL1
@@ -1118,11 +1118,11 @@ ORG CATDcode + P% - CATD
                         \
                         \ and either this is true:
                         \
-                        \   (r6^2 + r7^2) / 256 >= 16
+                        \   ((r6 + r7)^2 + r6^2) / 256 >= 16
                         \
                         \ or both these are true:
                         \
-                        \   (r6^2 + r7^2) / 256 < 16
+                        \   ((r6 + r7)^2 + r6^2) / 256 < 16
                         \   r5 >= 128
 
  LDA YY                 \ Set A = YY
@@ -1146,9 +1146,9 @@ ORG CATDcode + P% - CATD
                         \
                         \   32 <= ((r6 + r7)^2 + r5^2 + r6^2) / 256 < 80
                         \
-                        \   Either: (r6^2 + r7^2) / 256 >= 16
+                        \   Either: ((r6 + r7)^2 + r6^2) / 256 >= 16
                         \
-                        \   Or:     (r6^2 + r7^2) / 256 <  16
+                        \   Or:     ((r6 + r7)^2 + r6^2) / 256 <  16
                         \           r5 >= 128
                         \
                         \ which is what we want
