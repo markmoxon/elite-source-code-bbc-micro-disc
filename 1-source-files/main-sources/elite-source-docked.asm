929c7d7015226bf6f211595cfd68f8285c1d57d5
Mark Moxon <mark@moxon.net>
2024-04-18 22:56:11 +0100

    Fixed incorrect address range in K% workspace header

--- 1-source-files/main-sources/elite-source-docked.asm
+++ 1-source-files/main-sources/elite-source-docked.asm
@@ -1840,7 +1840,7 @@
 \
 \       Name: K%
 \       Type: Workspace
-\    Address: &0900 to &0D3F
+\    Address: &0900 to &0CFF
 \   Category: Workspaces
 \    Summary: Ship data blocks and ship line heaps
 \  Deep dive: Ship data blocks
@@ -1872,7 +1872,7 @@
 \
 \       Name: WP
 \       Type: Workspace
-\    Address: &0E00 to &0E3B
+\    Address: &0E00 to &0FD2
 \   Category: Workspaces
 \    Summary: Variables
 \
