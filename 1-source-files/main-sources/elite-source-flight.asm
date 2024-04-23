e73c63fa0c0e59032ef6bde3c621cdb75f4e8090
Mark Moxon <mark@moxon.net>
2024-04-23 17:03:16 +0100

    Remove scramble code from flight code

--- 1-source-files/main-sources/elite-source-flight.asm
+++ 1-source-files/main-sources/elite-source-flight.asm
@@ -2184,52 +2184,54 @@ ENDIF
 
 .DEEOR
 
-IF _STH_DISC OR _IB_DISC
+                        \ --- Mod: Code removed for Econet: ------------------->
 
- LDY #0                 \ We're going to work our way through a large number of
-                        \ encrypted bytes, so we set Y to 0 to be the index of
-                        \ the current byte within its page in memory
-
- STY SC                 \ Set the low byte of SC(1 0) to 0
-
- LDX #&13               \ Set X to &13 to be the page number of the current
-                        \ byte, so we start the decryption with the first byte
-                        \ of page &13
-
-.DEEORL
-
- STX SCH                \ Set the high byte of SC(1 0) to X, so SC(1 0) now
-                        \ points to the first byte of page X
-
- TYA                    \ Set A to Y, so A now contains the index of the current
-                        \ byte within its page
-
- EOR (SC),Y             \ EOR the current byte with its index within the page
-
- EOR #&33               \ EOR the current byte with &33
-
- STA (SC),Y             \ Update the current byte
-
-                        \ The current byte is in page X at offset Y, and SC(1 0)
-                        \ points to the first byte of page X, so we just did
-                        \  this:
-                        \
-                        \   (X Y) = (X Y) EOR Y EOR &33
-
- DEY                    \ Decrement the index in Y to point to the next byte
-
- BNE DEEORL             \ Loop back to DEEORL to decrypt the next byte until we
-                        \ have done the whole page
-
- INX                    \ Increment X to point to the next page in memory
-
- CPX #&56               \ Loop back to DEEORL to decrypt the next page until we
- BNE DEEORL             \ reach the start of page &56
-
- JMP RSHIPS             \ Call RSHIPS to launch from the station, load a new set
-                        \ of ship blueprints and jump into the main game loop
-
-ELIF _SRAM_DISC
+\IF _STH_DISC OR _IB_DISC
+\
+\LDY #0                 \ We're going to work our way through a large number of
+\                       \ encrypted bytes, so we set Y to 0 to be the index of
+\                       \ the current byte within its page in memory
+\
+\STY SC                 \ Set the low byte of SC(1 0) to 0
+\
+\LDX #&13               \ Set X to &13 to be the page number of the current
+\                       \ byte, so we start the decryption with the first byte
+\                       \ of page &13
+\
+\.DEEORL
+\
+\STX SCH                \ Set the high byte of SC(1 0) to X, so SC(1 0) now
+\                       \ points to the first byte of page X
+\
+\TYA                    \ Set A to Y, so A now contains the index of the current
+\                       \ byte within its page
+\
+\EOR (SC),Y             \ EOR the current byte with its index within the page
+\
+\EOR #&33               \ EOR the current byte with &33
+\
+\STA (SC),Y             \ Update the current byte
+\
+\                       \ The current byte is in page X at offset Y, and SC(1 0)
+\                       \ points to the first byte of page X, so we just did
+\                       \  this:
+\                       \
+\                       \   (X Y) = (X Y) EOR Y EOR &33
+\
+\DEY                    \ Decrement the index in Y to point to the next byte
+\
+\BNE DEEORL             \ Loop back to DEEORL to decrypt the next byte until we
+\                       \ have done the whole page
+\
+\INX                    \ Increment X to point to the next page in memory
+\
+\CPX #&56               \ Loop back to DEEORL to decrypt the next page until we
+\BNE DEEORL             \ reach the start of page &56
+\
+\JMP RSHIPS             \ Call RSHIPS to launch from the station, load a new set
+\                       \ of ship blueprints and jump into the main game loop
+\
+\ELIF _SRAM_DISC
 
                         \ --- And replaced by: -------------------------------->
 
@@ -2250,7 +2252,11 @@ ELIF _SRAM_DISC
  JMP RSHIPS             \ Call RSHIPS to launch from the station, load a new set
                         \ of ship blueprints and jump into the main game loop
 
-ENDIF
+                        \ --- Mod: Code removed for Econet: ------------------->
+
+\ENDIF
+
+                        \ --- End of removed code ----------------------------->
 
 \ ******************************************************************************
 \
