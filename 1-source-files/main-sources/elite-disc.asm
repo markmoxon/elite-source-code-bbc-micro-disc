0901648ec7925bef4c5dcf0fe8ebf7f19a924858
Mark Moxon <mark@moxon.net>
2026-01-15 13:46:07 +0000

    Replace sideways RAM loader with my own code that checks for existing ROMs

--- 1-source-files/main-sources/elite-disc.asm
+++ 1-source-files/main-sources/elite-disc.asm
@@ -77,4 +77,3 @@ ENDIF
  PUTFILE "1-source-files/boot-files/$.!BOOT.bin", "!BOOT", &FFFFFF, &FFFFFF
  PUTBASIC "1-source-files/music/load-music.bas", "ELITEM"
  PUTFILE "3-assembled-output/elite-music.rom", "MUSIC", &008000, &008000
- PUTFILE "1-source-files/music/SRLoad.bin", "SRLOAD", &FF0900, &FF093D
