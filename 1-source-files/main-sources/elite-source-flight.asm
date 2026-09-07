bda2cad7d053deea7f0185368b7c802beb9524e8
Mark Moxon <mark@moxon.net>
2026-09-07 12:52:06 +0100

    Add Moray bug fix

--- 1-source-files/main-sources/elite-source-flight.asm
+++ 1-source-files/main-sources/elite-source-flight.asm
@@ -26043,6 +26043,13 @@ ENDIF
  INC EV                 \ Increase the extra vessels spawning counter, to
                         \ prevent the next attempt to spawn extra vessels
 
+                        \ --- Mod: Code added for Moray bug fix: -------------->
+
+ LSR A                  \ Set the C flag randomly, so we sometimes spawn a Moray
+                        \ (see below)
+
+                        \ --- End of added code ------------------------------->
+
  AND #3                 \ Set A = random number in the range 0-3, which we
                         \ will now use to determine the type of ship
 
