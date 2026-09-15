e5e3d91d2eee50bf95923662d68a48bd68529b16
Mark Moxon <mark@moxon.net>
2026-09-15 16:10:00 +0100

    Fix more incorrect indents in commented-out code

--- 1-source-files/main-sources/elite-source-docked.asm
+++ 1-source-files/main-sources/elite-source-docked.asm
@@ -10205,17 +10205,17 @@ ENDIF
 
                         \ --- Mod: Code removed for Econet: ------------------->
 
-\ EQUB &8C, &E7
-\ EQUB &8D, &ED
-\ EQUB &8A, &E6
-\ EQUB &C1, &C8
-\ EQUB &C8, &8B
-\ EQUB &E0, &8A
-\ EQUB &E6, &D6
-\ EQUB &C5, &C6
-\ EQUB &C1, &CA
-\ EQUB &95, &9D
-\ EQUB &9C, &97
+\EQUB &8C, &E7
+\EQUB &8D, &ED
+\EQUB &8A, &E6
+\EQUB &C1, &C8
+\EQUB &C8, &8B
+\EQUB &E0, &8A
+\EQUB &E6, &D6
+\EQUB &C5, &C6
+\EQUB &C1, &CA
+\EQUB &95, &9D
+\EQUB &9C, &97
 
                         \ --- End of removed code ----------------------------->
 
